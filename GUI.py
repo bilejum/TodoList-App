ac104a779c00ad4773ef70c58669048904bdabdd
比乐喵 <17762591607@163.com>
2024-11-10 00:07:05 +0800

    fix IndexError

--- GUI.py
+++ GUI.py
@@ -8,15 +8,21 @@ list_box = sg.Listbox(
     values=functions.get_todo(), size=(45, 10), enable_events=True, key="List_Box"
 )
 edit_button = sg.Button("Edit", key="Edit")
+complete_button = sg.Button("Complete", key="Complete")
 
 window = sg.Window(
     title="ToDo List",
-    layout=[[Text_label], [todo_input, add_button], [list_box, edit_button]],
+    layout=[
+        [Text_label],
+        [todo_input, add_button],
+        [list_box, [edit_button, complete_button]],
+    ],
     font=("黑体", 14),
 )
 while True:
     event, values = window.read()
     print(values)
+    print(event)
     if event == sg.WIN_CLOSED or event == "Exit":
         break
 
@@ -27,11 +33,28 @@ while True:
         window["List_Box"].update(todos)
 
     if event == "Edit":
-        todos = functions.get_todo()
-        new_todo = values["Input"]
-        old_todo = values["List_Box"][0]
-        todo_index = todos.index(old_todo)
-        todos[todo_index] = new_todo + "\n"
-        functions.write_todo(todos=todos)
-        window["List_Box"].update(todos)
+        try:
+            todos = functions.get_todo()
+            new_todo = values["Input"]
+            old_todo = values["List_Box"][0]
+            todo_index = todos.index(old_todo)
+            todos[todo_index] = new_todo + "\n"
+            functions.write_todo(todos=todos)
+            window["List_Box"].update(todos)
+        except IndexError:
+            sg.popup("Please select a todo")
+
+    if event == "List_Box":
+        window["Input"].update(values["List_Box"][0].strip("\n"))
+
+    if event == "Complete":
+        try:
+            todos = functions.get_todo()
+            selected_todo = values["List_Box"][0]
+            print(selected_todo)
+            todos.remove(selected_todo)
+            functions.write_todo(todos)
+            window["List_Box"].update(todos)
+        except IndexError:
+            sg.popup("Please select a todo")
 window.close
